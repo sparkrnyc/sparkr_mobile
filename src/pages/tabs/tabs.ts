0a648298b684a96b44bf13aacdfcdcc2a19e52a7
Remko de Knikker <remkohdev@us.ibm.com@Remkos-MacBook-Pro.local>
2017-10-07 17:48:59 -0400

    updated some test data

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,10 +2,8 @@ import { Component, ViewChild } from '@angular/core';
 import { Tabs } from 'ionic-angular';
 import { Platform } from 'ionic-angular';
 
-import { FeedListPage } from '../feed/feed-list';
-import { TeamListPage } from '../team/team-list';
-import { ProjectListPage } from '../project/project-list';
 import { ProfileListPage } from '../profile/profile-list';
+import { TeamListPage } from '../team/team-list';
 import { ProfileDetailPage } from '../profile/profile-detail';
 
 @Component({
@@ -17,11 +15,9 @@ export class TabsPage {
   private isAndroid: boolean = false;
   @ViewChild('myTabs') myTabs: Tabs;
 
-  tab1Root = FeedListPage;//MySparkr
+  tab1Root = ProfileDetailPage;//MyProfile
   tab2Root = TeamListPage;//Teams
-  //tab3Root = ProjectListPage;//Projects
-  tab4Root = ProfileListPage;//Profiles
-  tab5Root = ProfileDetailPage;//Profile
+  tab3Root = ProfileListPage;//Profiles
 
   constructor(public platform: Platform) {
     this.isAndroid = this.platform.is('android');
--- src/providers/data/in-memory-db-test.ts
+++ src/providers/data/in-memory-db-test.ts
@@ -8,6 +8,7 @@ export class InMemoryDbTestProvider implements InMemoryDbService {
 
   createDb() {
 
+    // activity log 
     let feed = [
       {
         id : 1,
@@ -39,101 +40,149 @@ export class InMemoryDbTestProvider implements InMemoryDbService {
 
     let profile = [
       {
-        "id": "1",
-        "username": "remkohdev",
-        "thumbnail": "assets/imgs/person1.png",
-        "email": "remkohdev@gmail.com",
-        "linkedin": "https://linkedin.com/in/remkohdev/",
-        "firstname": "Remko",
-        "lastname": "de Knikker",
-        "city": "New York",
-        "state": "NY",
-        "country": "US",
-        "college": "",
-        "degree": "CS",
-        "company": "IBM",
-        "bio": "",
-        "role": "developer"
+        id: "1",
+        username: "remkohdev",
+        thumbnail: "assets/imgs/profiles/remkohdev.jpg",
+        email: "remkohdev@email.com",
+        linkedin: "https://linkedin.com/in/remkohdev/",
+        firstname: "Remko",
+        lastname: "de Knikker",
+        city: "New York",
+        state: "NY",
+        country: "US",
+        college: "Amsterdam University of Applied Sciences",
+        degree: "History",
+        company: "IBM",
+        bio: "",
+        role: "Developer"
       },
       {
-        "id": "2",
-        "username": "longemilyn",
-        "thumbnail": "assets/imgs/person2.png",
-        "email": "longemilyn@gmail.com",
-        "linkedin": "https://www.linkedin.com/in/emily-long-1a0a87b4/",
-        "firstname": "Emily",
-        "lastname": "Long",
-        "city": "",
-        "state": "NY",
-        "country": "US",
-        "college": "CUNY BMCC",
-        "degree": "CS",
-        "company": "",
-        "bio": "",
-        "role": "business lead"
+        id: "2",
+        username: "longemilyn",
+        thumbnail: "assets/imgs/profiles/longemilyn.jpg",
+        email: "longemilyn@email.com",
+        linkedin: "https://www.linkedin.com/in/emily-long-1a0a87b4/",
+        firstname: "Emily",
+        lastname: "Long",
+        city: "New York",
+        state: "NY",
+        country: "US",
+        college: "CUNY BMCC",
+        degree: "CS",
+        company: "Pivot",
+        bio: "Entrepreneur. Passionate about change. Advocate for Women in Technology.",
+        role: "Business"
       },
       {
-        "id": "3",
-        "username": "klantiislam",
-        "thumbnail": "assets/imgs/person3.png",
-        "email": "",
-        "linkedin": "https://www.linkedin.com/in/klantiislam/",
-        "firstname": "Klanti",
-        "lastname": "Islam",
-        "city": "",
-        "state": "",
-        "country": "",
-        "college": "CUNY Brooklyn College",
-        "degree": "AA",
-        "company": "",
-        "bio": "",
-        "role": "developer"
+        id: "3",
+        username: "klantiislam",
+        thumbnail: "assets/imgs/profiles/klantiislam.jpg",
+        email: "klantiislam@email.com",
+        linkedin: "https://www.linkedin.com/in/klantiislam/",
+        firstname: "Klanti",
+        lastname: "Islam",
+        city: "New York",
+        state: "NY",
+        country: "US",
+        college: "CUNY Brooklyn College",
+        degree: "AA",
+        company: "",
+        bio: "Active Member at Women in Technology and Entrepreneurship (WiTNY)",
+        role: "Developer"
       },
       {
-        "id": "4",
-        "username": "aavetisian",
-        "thumbnail": "assets/imgs/person4.png",
-        "email": "",
-        "linkedin": "https://www.linkedin.com/in/albert-avetisian/",
-        "firstname": "Albert",
-        "lastname": "Avetisian",
-        "city": "",
-        "state": "NY",
-        "country": "US",
-        "college": "CUNY Baruch",
-        "degree": "CS",
-        "company": "IBM",
-        "bio": "Design minded developer of web applications, experiences, and businesses. Working daily to realize designs into tangible prototypes, solutions, and tools for the enterprise in the form of web and hybrid mobile applications. Besides developing interfaces, I work with schools like Bayside High School to bring advanced technical education and opportunities to better prepare students for their careers.",
-        "role": "developer"
-      }
-    ];
-
-    let project = [
+        id: "4",
+        username: "aavetisian",
+        thumbnail: "assets/imgs/profiles/albert.jpg",
+        email: "aavetis@email.com",
+        linkedin: "https://www.linkedin.com/in/albert-avetisian/",
+        firstname: "Albert",
+        lastname: "Avetisian",
+        city: "Westchester",
+        state: "NY",
+        country: "US",
+        college: "CUNY Baruch",
+        degree: "CS",
+        company: "IBM",
+        bio: "Design minded developer of web applications, experiences, and businesses. Working daily to realize designs into tangible prototypes, solutions, and tools for the enterprise in the form of web and hybrid mobile applications. Besides developing interfaces, I work with schools like Bayside High School to bring advanced technical education and opportunities to better prepare students for their careers.",
+        role: "Developer"
+      },
       {
-        "id" : 1,
-        "title" : "Hacking App",
-        "description" : "Some kind of sample app that we want to make at the upcoming CUNY Hackathon. Please help!",
-        "url": "",
-        "prototype": ""
+        id: "5",
+        username: "olga",
+        thumbnail: "assets/imgs/profiles/olga.jpg",
+        email: "olga@email.com",
+        linkedin: "https://www.linkedin.com/in/olgabartnicki/",
+        firstname: "Olga",
+        lastname: "Bartnicki",
+        city: "",
+        state: "NY",
+        country: "US",
+        college: "Harvard",
+        degree: "",
+        company: "CUNY Startups",
+        bio: "Innovative and decisive leader with extensive experience in launching startups from idea to tangible revenue-generating businesses, and launching new ventures within established companies.",
+        role: "ProjectManager"
+      },
+      {
+        id: "6",
+        username: "shanesnipes1",
+        thumbnail: "assets/imgs/profiles/shanesnipes1.jpg",
+        email: "shanesnipes1@email.com",
+        linkedin: "https://www.linkedin.com/in/shanesnipes/",
+        firstname: "R. Shane",
+        lastname: "Snipes",
+        city: "Queens",
+        state: "NY",
+        country: "US",
+        college: "Prescott College",
+        degree: "PhD Ed",
+        company: "CUNY BMCC",
+        bio: "I'm a professor/facilitator in academia, as well as a HR and innovation strategist for organizations. I'm expert in business people systems, innovation labs, and process redesign. I have worn many hats in my career—college faculty, chief technology officer, TV personality, innovation manager, HR executive, CEO, and education consultant. As a result, I have a unique ability to manage projects and to navigate complex challenges.",
+        role: "ProjectManager"
       }
     ];
 
     let team = [
       {
-        "id" : 1,
-        "name" : "d0pe hAckers_01",
-        "thumbnail": "assets/imgs/team_icon_1.png",
-        "description" : "Some kind of sample team that intended to work at the upcoming CUNY Hackathon. Super talented team that does..uh..hacking.",
-        "members": [
+        id : 1,
+        name : "d0pe hAckers_01",
+        thumbnail: "assets/imgs/team_icon_1.png",
+        description : "Some kind of sample team that intended to work at the upcoming CUNY Hackathon. Super talented team that does..uh..hacking.",
+        members: [
           1,2
         ],
-        "projects": [
+        projects: [
           1
+        ]
+      },
+      {
+        id : 1,
+        name : "The Russian Election Campaignteam",
+        thumbnail: "assets/imgs/team_icon_2.png",
+        description : "Trying to hack foreign elections and place puppet presidents in power.",
+        members: [
+          1,3
+        ],
+        projects: [
+          2
+        ]
+      },
+      {
+        id : 3,
+        name : "SPARKR",
+        thumbnail: "assets/imgs/teams/sparkr.png",
+        description : "Thinking about going to a Hacakthon in NYC? Want to learn some new skills and test the waters? But do not have a team? Use SPARKR to let others know what you are good at and to find other team members.",
+        members: [
+          1,2,4,5,6
+        ],
+        projects: [
+          3
         ]
       }
     ];
     
-    return { feed, notification, profile, project, team };
+    return { feed, notification, profile, team };
   }
   
 }
